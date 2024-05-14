002cb02c225b6ab1bd141313a26dd2ed9fe69276
ody <rurii@knou.ac.kr>
2024-05-15 02:57:27 +0900

    스프링부트 강좌 14강(블로그 프로젝트) - http요청 실습2

--- src/main/java/com/example/blog/test/HttpControllerTest.java
+++ src/main/java/com/example/blog/test/HttpControllerTest.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // 사용자가 요청 -> 응답(HTML 파일)
@@ -14,18 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class HttpControllerTest {
 
 	@GetMapping("/http/get")
-	public String getTest() {
-		return "get 요청";
+	public String getTest(Member m) {
+	    return "get 요청 : " + m.getId()+", "+m.getUsername()+","+m.getPassword()+","+m.getEmail();
 	}
 
 	@PostMapping("/http/post")
-	public String postTest() {
-		return "post 요청";
+	public String postTest(@RequestBody Member m) { //MessageConverter (스프링부트)
+		return "get 요청 : " + m.getId()+", "+m.getUsername()+","+m.getPassword()+","+m.getEmail();
 	}
 	
 	@PutMapping("/http/put")
-	public String putTest() {
-		return "put 요청";
+	public String putTest(@RequestBody Member m) {
+		return "put 요청"+ m.getId()+", "+m.getUsername()+","+m.getPassword()+","+m.getEmail();
 	}
 	
 	@DeleteMapping("/http/delete")
---
+++ src/main/java/com/example/blog/test/People.java
@@ -0,0 +1,10 @@
+package com.example.blog.test;
+
+public class People {
+	int hungryState = 50; // 100
+	
+	public void eat() {
+		hungryState += 10;
+	}
+	
+}
