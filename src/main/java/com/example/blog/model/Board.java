1f7f0be0bc4e38c2b7887c50008f2a366019674c
ody <rurii@knou.ac.kr>
2024-05-15 21:20:40 +0900

    스프링부트 강좌 22강(블로그 프로젝트) - 연관관계의 주인

--- src/main/java/com/example/blog/model/Board.java
+++ src/main/java/com/example/blog/model/Board.java
@@ -1,18 +1,21 @@
 package com.example.blog.model;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -38,10 +41,16 @@ public class Board {
 	@ColumnDefault("0")
 	private int count; // 조회수
 
-	@ManyToOne // Many = Board, User = One 한명의 유저는 여러개의 게시물을 쓸 수 있다.
+	@ManyToOne(fetch= FetchType.EAGER) // Many = Board, User = One 한명의 유저는 여러개의 게시물을 쓸 수 있다.
 	@JoinColumn(name = "appUserId")
 	private AppUser appUser; // DB는 오브젝트를 저장할 수 없다. FK, 자바는 오브젝트를 저장할 수 있다.
 	// 자동으로 FK가 만들어짐
+
+	@OneToMany(mappedBy = "board", fetch = FetchType.EAGER)
+	// mappedBy 연관관게의 주인이 아니다 (난 FK키가 아니예요)
+	// DB에 컬럼을 만들지 마세요
+	// board를 셀렉트할떄 join문을 위해 값을 얻기 위해 필요한 겁니다.
+	private List<Reply> reply;
 
 	@CreationTimestamp
 	private Timestamp createDate;
