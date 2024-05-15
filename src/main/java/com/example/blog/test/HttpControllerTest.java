cfb1edcdf12fb8c583144c95161d82a8b16dfdc5
ody <rurii@knou.ac.kr>
2024-05-15 15:59:05 +0900

    16강 lombok

--- src/main/java/com/example/blog/test/HttpControllerTest.java
+++ src/main/java/com/example/blog/test/HttpControllerTest.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // 사용자가 요청 -> 응답(HTML 파일)
@@ -14,9 +13,24 @@ import org.springframework.web.bind.annotation.RestController;
 // 사용자가 요청 -> 응답(Data)
 @RestController
 public class HttpControllerTest {
+	
+	private static final String TAG= "HttpControllerTest : ";
+	
+	@GetMapping("/https/lombok")
+	public String lombokTest() {
+		// builder패턴쓰면 생성자의 순서 안지켜도 됨!!
+		Member m = Member.builder().username("ssar").password("1234").email("ssar@nate").build();
+		System.out.println(TAG+"getter :"+m.getUsername());
+		m.setUsername("cos");
+		System.out.println(TAG+"setter :"+m.getUsername());
+		return "lombok test 완료";
+	}
 
 	@GetMapping("/http/get")
 	public String getTest(Member m) {
+		System.out.println(TAG+"getter : " +m.getId());
+		m.setId(5000);
+		System.out.println(TAG+"setter :  + m.getId())");
 	    return "get 요청 : " + m.getId()+", "+m.getUsername()+","+m.getPassword()+","+m.getEmail();
 	}
 
