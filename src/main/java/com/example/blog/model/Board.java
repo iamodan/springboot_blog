a5f7cba685448f1634c9123a00245d543080854f
ody <rurii@knou.ac.kr>
2024-05-15 20:29:14 +0900

    스프링부트 강좌 21강(블로그 프로젝트) Reply 테이블 생성

--- src/main/java/com/example/blog/model/Board.java
+++ src/main/java/com/example/blog/model/Board.java
@@ -1,5 +1,49 @@
 package com.example.blog.model;
 
+import java.sql.Timestamp;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data // Getter + Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder // 빌더 패턴
+@Entity
 public class Board {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
+	private int id;
+
+	@Column(nullable = false, length = 100)
+	private String title;
+
+	@Lob // 대용량 데이터
+	private String content; // 섬머노트 라이브러리 <html>태그가 섞여서 디자인 됨.
+
+	@ColumnDefault("0")
+	private int count; // 조회수
+
+	@ManyToOne // Many = Board, User = One 한명의 유저는 여러개의 게시물을 쓸 수 있다.
+	@JoinColumn(name = "appUserId")
+	private AppUser appUser; // DB는 오브젝트를 저장할 수 없다. FK, 자바는 오브젝트를 저장할 수 있다.
+	// 자동으로 FK가 만들어짐
+
+	@CreationTimestamp
+	private Timestamp createDate;
 
 }
--- src/main/java/com/example/blog/model/Reply.java
+++ src/main/java/com/example/blog/model/Reply.java
@@ -1,5 +1,44 @@
 package com.example.blog.model;
 
+import java.sql.Timestamp;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data // Getter + Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder // 빌더 패턴
+@Entity
 public class Reply {
 
+	@Id // Primary key
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	// 프로젝트에서 연결된 DB의 넘버링 전략을 따라나다
+	private int id; // 시퀀스, auto_increment
+
+	@Column(nullable = false, length = 200)
+	private String content; 
+	
+	@ManyToOne
+	@JoinColumn(name="boardId")
+	private Board board;
+	
+	@ManyToOne
+	@JoinColumn(name="AppUserId")
+	private AppUser appUser;
+
+	@CreationTimestamp
+	private Timestamp createDate;
 }
